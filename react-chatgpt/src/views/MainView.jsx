254a0734cee0d30bb8ea91acc05eb75f60f0ba0d
Naoyuki <zhaonzh@gmail.com>
2023-05-14 02:09:28 -0700

    change css

--- react-chatgpt/src/views/MainView.jsx
+++ react-chatgpt/src/views/MainView.jsx
@@ -4,7 +4,7 @@ import { Dialogs } from "../components/dialog"
 import { Input } from "../components/input"
 import otter from "../assets/otter.png"
 import React, { useState, useContext } from "react"
-import {OtterContext,useOtterSettings} from "../data/otter"
+import { OtterContext, useOtterSettings } from "../data/otter"
 import "../styles/global.css"
 import chat from "../assets/chat.svg"
 import game from "../assets/game.svg"
@@ -20,35 +20,39 @@ const switchState = (state) => {
 const MainView = () => {
 
     const [showForm, setShowForm] = useState(false);
-    const { health, credit, changeHealth} = useOtterSettings();
+    const { health, credit, changeHealth } = useOtterSettings();
 
     return (
-            <div id="bg">
-                <div className="flex-container">
-                    <CreditsBar c_level={credit}></CreditsBar>
-                    <div className="flex-container-fill"></div>
-                    <HealthBar h_level={health}></HealthBar>
-                </div>
-                <Dialogs></Dialogs>
-
-                {showForm ? <Input></Input> : null}
-
-                <img style={{ "maxHeight": "300px", "margin": "0 auto", "display": "block" }} src={otter} alt=""></img>
-                <div className="flex-container button-list">
-                    <button className="game-button a" onClick={() => {
-                        setShowForm(switchState(showForm));
-                    }}>
-                        <img src={chat}></img>
-                    </button>
-                    <button className="game-button b">
-                        <img src={game}></img>
-                    </button>
-                    <button className="game-button c">
-                        <img src={food}></img>
-                    </button>
-                </div>
+        <div id="bg">
+            <div className="flex-container">
+                <CreditsBar c_level={credit}></CreditsBar>
+                <div className="flex-container-fill"></div>
+                <HealthBar h_level={health}></HealthBar>
             </div>
-        )
+
+
+            <div className="flex-container">
+                <Dialogs></Dialogs>
+                <div className="flex-container-fill"></div>
+                {showForm ? <Input></Input> : null}
+            </div>
+
+            <img style={{ "maxHeight": "300px", "margin": "0 auto", "display": "block" }} src={otter} alt=""></img>
+            <div className="flex-container button-list">
+                <button className="game-button a" onClick={() => {
+                    setShowForm(switchState(showForm));
+                }}>
+                    <img src={chat}></img>
+                </button>
+                <button className="game-button b">
+                    <img src={game}></img>
+                </button>
+                <button className="game-button c">
+                    <img src={food}></img>
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export { MainView };
